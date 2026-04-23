ebbd7083d6df941a61d4beb0f30780b554106448
wangjh <wangjianhui2003@outlook.com>
2026-04-23 16:20:57 +0800

    update nvim

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,18 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- 如果是 fcitx5 或 ibus，按了 ESC 后自动切回英文输入法
+local input_method_group = vim.api.nvim_create_augroup("user_input_method", { clear = true })
+
+vim.api.nvim_create_autocmd("InsertLeave", {
+  group = input_method_group,
+  desc = "Switch to English input method when leaving insert mode",
+  callback = function()
+    if vim.fn.executable("fcitx5-remote") == 1 then
+      vim.fn.jobstart({ "fcitx5-remote", "-c" }, { detach = true })
+    elseif vim.fn.executable("ibus") == 1 then
+      vim.fn.jobstart({ "ibus", "engine", "xkb:us::eng" }, { detach = true })
+    end
+  end,
+})
